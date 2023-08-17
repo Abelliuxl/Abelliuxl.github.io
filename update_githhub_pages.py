d6587d76aefd019026f0508f1cd8e1b116fa09bc
liuxxl <liuxxl@liuxlsys>
2023-08-17 14:12:35 +0800

    自动提交更改

--- update_githhub_pages.py
+++ update_githhub_pages.py
@@ -6,9 +6,8 @@ repo_path = '/home/liuxxl/Abelliuxl.github.io'
 # 提交的消息
 commit_message = '自动提交更改'
 
-# GitHub 账户凭据
-username = 'Abelliuxl'
-password = '131415978DALIANG'
+# GitHub 个人访问令牌
+access_token = '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'
 
 # 打开 Git 仓库
 repo = Repo(repo_path)
@@ -16,7 +15,9 @@ repo = Repo(repo_path)
 # 配置远程仓库
 origin = repo.remote(name='origin')
 origin_url = origin.url
-new_origin_url = origin_url.replace('https://', f'https://{username}:{password}@')
+
+# 将访问令牌添加到远程仓库 URL
+new_origin_url = origin_url.replace('https://', f'https://{access_token}@')
 repo.git.remote('set-url', 'origin', new_origin_url)
 
 # 获取当前分支
